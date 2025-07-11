59fa8809da02ce3a1fa04bc5edd1669b47959777
ducbcthilogi <ducbc.thilogi@gmail.com>
2025-07-11 08:29:47 +0700

    aa

---
+++ frontend/src/components/Footer copy 2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2025 NetCore Solutions Co., Ltd. All rights reserved.</p>
+        <p>Công ty TNHH MTV Giải Pháp Công Nghệ NetCore Solutions</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
